749ae73a8261cd9f972bedfca11eb9ff21fb933c
Azizjon <aziz.nigmatjonov7@gmail.com>
2021-02-01 13:34:22 +0500

    updated

--- m.js
+++ m.js
@@ -1,5 +1,5 @@
 const bodyElement = document.querySelector('body');
-console.log(window.location)
+
 const wrapperElement = document.querySelector('.wrapper');
 const checkElement1 = document.querySelector('.stone-form .form-input');
 const checkElement2 = document.querySelector('.scissors-form .form-input');
@@ -13,23 +13,80 @@ const stoneElement = document.querySelector('.image-stone');
 const scissorsElement = document.querySelector('.image-scissors');
 const paperElement = document.querySelector('.image-paper');
 
+// create and unappear same stone element
+const secondImageStone = document.querySelector('.stone-img_2');
+const secondStoneElement = document.querySelector('.stone_2');
+secondStoneElement.style.display = 'none';
+secondImageStone.style.display = 'none';
+
+// create and unappear same scissors element
+const secondScissorsElement = document.querySelector('.scissors_2');
+const secondImageScissors = document.querySelector('.scissors-img_2')
+secondScissorsElement.style.display = 'none';
+secondImageScissors.style.display = 'none';
+
+// create and unappear same paper element
+const secondPaperElement = document.querySelector('.paper_2');
+const secondImagePaper = document.querySelector('.paper-img_2')
+secondPaperElement.style.display = 'none';
+secondImagePaper.style.display = 'none';
+
 // stone
 checkElement1.addEventListener('change', function(e){
     if (checkElement1.checked) {
         checkElement1.value = 1;
         // random image
-        function appendToArray(img2, img3) {
+        function appendToArray(img1,img2, img3) {
             let newArray = []
-            newArray.push(img2,img3);
+            newArray.push(img1,img2,img3);
             
             let array1 = newArray[0];
             let array2 = newArray[1];
+            let array3 = newArray[2];
 
             if (array1 == newArray[Math.floor(Math.random() * newArray.length)]){
+                img2.style.display = 'none';
+                scissorsElement.style.display = 'none';
                 img3.style.display = 'none';
                 paperElement.style.display = 'none';
 
+                // create same elements
+                secondStoneElement.style.display = 'initial';
+                secondImageStone.style.display = 'initial';
+
                 // create congretulations
+                let result = document.createElement('div');
+                result.classList.add('result');
+                wrapperElement.appendChild(result);
+                let resultTextYesElement = document.createElement('p');
+                resultTextYesElement.textContent = 'Durang!';
+                result.appendChild(resultTextYesElement);
+                resultTextYesElement.classList.add('result-text');
+
+                // request to refresh 
+                const requestElement = document.createElement('p');
+                requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
+                requestElement.classList.add('game-reference');
+                bodyElement.prepend(requestElement);
+
+                // stop game and refresh
+                const refreshButton = document.createElement('button');
+                refreshButton.classList.add('refresh');
+                wrapperElement.prepend(refreshButton);
+                const refreshButtonText = document.createElement('p');
+                refreshButtonText.textContent = `O'yinni yangilash.`;
+                refreshButtonText.classList.add('refresh-text');
+                refreshButton.appendChild(refreshButtonText);
+                refreshButton.addEventListener('click', function(e){
+                    window.location.href = "file:///C:/webdev/Lessons/web_lessons/lesson_12_JS/index.html";
+                })
+            } else if (array2 == newArray[Math.floor(Math.random() * newArray.length)]){
+                img3.style.display = 'none';
+                paperElement.style.display = 'none';
+                img2.style.display = 'initial';
+                scissorsElement.style.display = 'initial';
+
+                 // create congretulations
                 let result = document.createElement('div');
                 result.classList.add('result');
                 wrapperElement.appendChild(result);
@@ -88,7 +145,7 @@ checkElement1.addEventListener('change', function(e){
                 })
             }
         }
-        appendToArray(imageElement2,imageElement3)
+        appendToArray(imageElement1,imageElement2,imageElement3)
     } else {
         checkElement1.value = 0;
     }
@@ -100,24 +157,30 @@ checkElement2.addEventListener('change', function(e){
     if (checkElement2.checked) {
         checkElement2.value = 1;
         // random image
-        function appendToArray(img1, img3) {
+        function appendToArray(img1,img2, img3) {
             let newArray = []
-            newArray.push(img1,img3);
-            
+            newArray.push(img1,img2,img3);
+
             let array1 = newArray[0];
             let array2 = newArray[1];
+            let array3 = newArray[2];
 
-            if (array1 == newArray[Math.floor(Math.random() * newArray.length)]){
+            if (array2 == newArray[Math.floor(Math.random() * newArray.length)]){
                 img3.style.display = 'none';
                 paperElement.style.display = 'none';
+                img1.style.display = 'none';
+                stoneElement.style.display = 'none';
 
+                // create same elements 
+                secondScissorsElement.style.display = 'initial';
+                secondImageScissors.style.display = 'initial';
 
                 // create congretulations
                 let result = document.createElement('div');
                 result.classList.add('result');
                 wrapperElement.appendChild(result);
                 let resultTextYesElement = document.createElement('p');
-                resultTextYesElement.textContent = 'Afsus siz yutqazdingiz!';
+                resultTextYesElement.textContent = 'Durang!';
                 result.appendChild(resultTextYesElement);
                 resultTextYesElement.classList.add('result-text');
 
@@ -126,7 +189,7 @@ checkElement2.addEventListener('change', function(e){
                 requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
                 requestElement.classList.add('game-reference');
                 bodyElement.prepend(requestElement);
-                
+
                 // stop game and refresh
                 const refreshButton = document.createElement('button');
                 refreshButton.classList.add('refresh');
@@ -139,6 +202,38 @@ checkElement2.addEventListener('change', function(e){
                     window.location.href = "file:///C:/webdev/Lessons/web_lessons/lesson_12_JS/index.html";
                 })
 
+            } else if (array1 == newArray[Math.floor(Math.random() * newArray.length)]){
+                img3.style.display = 'none';
+                paperElement.style.display = 'none';
+                img1.style.display = 'initial';
+                stoneElement.style.display = 'initial';
+
+                 // create congretulations
+                let result = document.createElement('div');
+                result.classList.add('result');
+                wrapperElement.appendChild(result);
+                let resultTextYesElement = document.createElement('p');
+                resultTextYesElement.textContent = 'Afus siz yutqazdingiz!';
+                result.appendChild(resultTextYesElement);
+                resultTextYesElement.classList.add('result-text');
+
+                // request to refresh 
+                const requestElement = document.createElement('p');
+                requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
+                requestElement.classList.add('game-reference');
+                bodyElement.prepend(requestElement);
+
+                // stop game and refresh
+                const refreshButton = document.createElement('button');
+                refreshButton.classList.add('refresh');
+                wrapperElement.prepend(refreshButton);
+                const refreshButtonText = document.createElement('p');
+                refreshButtonText.textContent = `O'yinni yangilash.`;
+                refreshButtonText.classList.add('refresh-text');
+                refreshButton.appendChild(refreshButtonText);
+                refreshButton.addEventListener('click', function(e){
+                    window.location.href = "file:///C:/webdev/Lessons/web_lessons/lesson_12_JS/index.html";
+                })
             } else {
                 img3.style.display = 'initial';
                 img1.style.display = 'none';
@@ -158,7 +253,7 @@ checkElement2.addEventListener('change', function(e){
                 requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
                 requestElement.classList.add('game-reference');
                 bodyElement.prepend(requestElement);
-                
+
                 // stop game and refresh
                 const refreshButton = document.createElement('button');
                 refreshButton.classList.add('refresh')
@@ -172,30 +267,67 @@ checkElement2.addEventListener('change', function(e){
                 })
             }
         }
-        appendToArray(imageElement1,imageElement3)
+        appendToArray(imageElement1,imageElement2,imageElement3)
     } else {
         checkElement1.value = 0;
     }
     // console.log(checkElement1.value);
 })
 
+// paper
 checkElement3.addEventListener('change', function(e){
       if (checkElement3.checked) {
         checkElement3.value = 1;
         // random image
-        function appendToArray(img1, img2) {
+        function appendToArray(img1, img2,img3) {
             let newArray = []
-            newArray.push(img1,img2);
-            
+            newArray.push(img1,img2,img3);
+
             let array1 = newArray[0];
             let array2 = newArray[1];
+            let array3 = newArray[2];
 
-            if (array1 == newArray[Math.floor(Math.random() * newArray.length)]){
+            if (array3 == newArray[Math.floor(Math.random() * newArray.length)]){
                 img2.style.display = 'none';
                 scissorsElement.style.display = 'none';
+                img1.style.display = 'none';
+                stoneElement.style.display = 'none';
 
+                // create same elements
+                secondPaperElement.style.display = 'initial';
+                secondImagePaper.style.display = 'initial';
 
                 // create congretulations
+                let result = document.createElement('div');
+                result.classList.add('result');
+                wrapperElement.appendChild(result);
+                let resultTextYesElement = document.createElement('p');
+                resultTextYesElement.textContent = 'Durang!';
+                result.appendChild(resultTextYesElement);
+                resultTextYesElement.classList.add('result-text');
+
+                // request to refresh 
+                const requestElement = document.createElement('p');
+                requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
+                requestElement.classList.add('game-reference');
+                bodyElement.prepend(requestElement);
+
+                // stop game and refresh
+                const refreshButton = document.createElement('button');
+                refreshButton.classList.add('refresh')
+                wrapperElement.prepend(refreshButton)
+                const refreshButtonText = document.createElement('p');
+                refreshButtonText.textContent = `O'yinni yangilash.`;
+                refreshButtonText.classList.add('refresh-text');
+                refreshButton.appendChild(refreshButtonText);
+                refreshButton.addEventListener('click', function(e){
+                    window.location.href = "file:///C:/webdev/Lessons/web_lessons/lesson_12_JS/index.html";
+                })
+
+            } else if (array2 == newArray[Math.floor(Math.random() * newArray.length)]){
+                img2.style.display = 'none';
+                scissorsElement.style.display = 'none';
+                 // create congretulations
                 let result = document.createElement('div');
                 result.classList.add('result');
                 wrapperElement.appendChild(result);
@@ -209,7 +341,7 @@ checkElement3.addEventListener('change', function(e){
                 requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
                 requestElement.classList.add('game-reference');
                 bodyElement.prepend(requestElement);
-                
+
                 // stop game and refresh
                 const refreshButton = document.createElement('button');
                 refreshButton.classList.add('refresh')
@@ -221,7 +353,6 @@ checkElement3.addEventListener('change', function(e){
                 refreshButton.addEventListener('click', function(e){
                     window.location.href = "file:///C:/webdev/Lessons/web_lessons/lesson_12_JS/index.html";
                 })
-
             } else {
                 img2.style.display = 'initial';
                 img1.style.display = 'none';
@@ -241,7 +372,7 @@ checkElement3.addEventListener('change', function(e){
                 requestElement.textContent = `O'yin yakunlangach yangitdan o'ynash uchun "O'yinni yangilash" tugmasini bosing!`;
                 requestElement.classList.add('game-reference');
                 bodyElement.prepend(requestElement);
-                
+
                 // stop game and refresh
                 const refreshButton = document.createElement('button');
                 refreshButton.classList.add('refresh');
@@ -254,7 +385,7 @@ checkElement3.addEventListener('change', function(e){
                     window.location.href = "file:///C:/webdev/Lessons/web_lessons/lesson_12_JS/index.html";                })
             }
         }
-        appendToArray(imageElement1,imageElement2)
+        appendToArray(imageElement1,imageElement2,imageElement3)
     } else {
         checkElement1.value = 0;
     }
